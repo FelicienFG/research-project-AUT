fixed learning algorithm, found makespan calculation not differentiable

code/ml_model/makespan_loss.py
@@ -11,14 +11,21 @@ class MakespanLoss(torch.nn.Module):
 
     def forward(self, dagTask, output, target_makespan):
         """
-        the output is assumed to be a matrix of scores with the subtasks' ids as rows and priorities as columns
-        the target is assumed to be the ILP optimal calculation of the makespan
+        the output is assumed to be a batch of matrices of scores with the subtasks' ids as rows and priorities as columns
+        the target is assumed to be the batch of ILP optimal calculations of the makespan
         """
 
-        _, prio_list = torch.max(output, dim=1)
-        makespan = self.makespanSolver.computeMakespan(ms.IntList(prio_list), dagTask)
-        
-        return 1. - makespan / target_makespan
+        accu_loss = 0.0
+        #print(output)
+        for matrix_index in range(output.shape[0]):
+
+            _, prio_list = torch.max(output[matrix_index], dim=1)
+            #problem, makespan calculation not differentiable, IDEA: forward pass outputs the schedule instead of just a priority list
+            makespan = 1#self.makespanSolver.computeMakespan(ms.IntVector(prio_list.tolist()), dagTask[matrix_index])
+            
+            accu_loss += 1. - makespan / target_makespan[matrix_index]
+
+        return accu_loss
 
 
 

code/ml_model/ml_model.py
@@ -109,10 +109,11 @@ def train_one_epoch(model, epoch_num, batches, dataLoader, optimizer, loss_fn):
         #reset gradients
         optimizer.zero_grad()
         
-        outputs = model(dataLoader.taskFeatures[batch])
-
+        outputs, _ = model(dataLoader.taskFeatures[batch])
+        
         #compute loss and gradients
         loss = loss_fn([dataLoader.dagTasks[i] for i in batch], outputs, dataLoader.ilpOutputs[batch])
+        loss.requires_grad = True
         loss.backward()
 
         optimizer.step()
@@ -148,7 +149,7 @@ def training_and_eval(model, num_cores):
 
         # Disable gradient computation and reduce memory consumption.
         with torch.no_grad():
-            voutputs = model(valTaskFeatures)
+            voutputs, _ = model(valTaskFeatures)
             vloss = loss_fn(valDagTasks, voutputs, valILPoutputs)
             avg_vloss = vloss / len(valDagTasks)
 