3f7272ff08d974bd8f14903c676cc2b0c9f52ff2
alphoenix <felicien.fiscus@ensiie.fr>
2024-10-11 15:43:57 +0200

    added script to compute last schedules

--- code/LET-LP-Scheduler/compute_last_few_schedules.sh
+++ code/LET-LP-Scheduler/compute_last_few_schedules.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-input_dir="dag_tasks_input_files"
-output_dir="dag_tasks_output_schedules"
 
-dag_ids="112 113 114 115 116" 
 
-for dag_task_id in $dag_ids ; 
+
+p=8
+n_list="10 20 30"
+m_list="6 7 8"
+
+
+for m in $m_list ;
 do
-    python3 main.py --infile "$input_dir/multicore_system_DAGtask_$dag_task_id.json" --outfile "$output_dir/schedule_dag_$dag_task_id.json" --solver GUROBI ;
+    for n in $n_list ;
+    do
+        dag_ids="cat missing_file_ids_m${m}p${p}n${n}" 
+        inputDir="dag_m${m}p${p}n${n}_input_files"
+        outputDir="dag_m${m}p${p}n${n}_output_schedules"
+
+        for dag_task_id in $dag_ids ; 
+        do
+                python3 main.py --infile "$inputDir/multicore_system_DAGtask_$dag_task_id.json" --outfile "$outputDir/schedule_dag_$dag_task_id.json" --solver GUROBI > /tmp/${outputDir}DAG_${dag_task_id}.log &
+        done
+    done
 done
