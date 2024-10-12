64f3063e731f2d60998a5f2c6e0fc86173b8832f
felicien fiscus <felicien.fgay@gmail.com>
2024-10-12 15:39:00 +0000

    remobed modification

--- code/LET-LP-Scheduler/save_every_so_often.sh
+++ code/LET-LP-Scheduler/save_every_so_often.sh
@@ -4,8 +4,8 @@
 i=0
 while :
 do
-  #backup every 45 min
-  sleep 2700;
+  #backup every half an hour
+  sleep 1800;
   git add . ;
   git commit -m "model train and eval save number $i" ;
   git push ;
